2a32a704a0c10827ed281105770948f4afd187c9
Maureen-J-kirotich <mkirotich@kabarak.ac.ke>
2022-02-27 22:14:02 +0300

    Added a new folder 'project' it contains my class projects on Stadia Management system

---
+++ Project/images/Kenya Fixtures SuperSport – Africa’s source of sports video, fixtures, results and news_files/em.js
@@ -0,0 +1,9 @@
+// Effective Measure
+(function () {
+    var em = document.createElement('script');
+    em.type = 'text/javascript';
+    em.async = true;
+    em.src = ('https:' == document.location.protocol ? 'https://za-ssl' : 'http://za-cdn') + '.effectivemeasure.net/em.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(em, s);
+})();
